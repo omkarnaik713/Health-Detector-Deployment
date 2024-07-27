0fb89ab44623828d3fbd43854974a83c92e3a026
omkarnaik713 <76861864+omkarnaik713@users.noreply.github.com>
2024-07-26 20:00:23 -0400

    Assigning an open port

--- app.py
+++ app.py
@@ -72,4 +72,5 @@ def predict():
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0',port = 8080, ssl_context = 'adhoc')
+    port = int(os.environ.get('PORT', 8080))
+    app.run(host='0.0.0.0',port = port, ssl_context = 'adhoc')
