715277e54cd0332d4da86b247b87868b5473b858
omkarnaik713 <76861864+omkarnaik713@users.noreply.github.com>
2024-07-27 10:10:25 -0400

    Updating the ping function

--- app.py
+++ app.py
@@ -20,7 +20,7 @@ model = pickle.load(open('nn_model.pkl','rb'))
 app.config['upload_folder'] = '/var/log/uploads'
 uploads_folder = '/app/upload'
 
-@app.route('/ping', methods = ['GET'])
+@app.route('/ping', methods = ['GET','HEAD'])
 def ping():
     return 'OK', 200
 
