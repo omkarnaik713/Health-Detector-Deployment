7835e905d4410798b93590efddf3698aa87f987f
omkarnaik713 <76861864+omkarnaik713@users.noreply.github.com>
2024-07-26 21:14:58 -0400

    removing specific port and ssl context

--- app.py
+++ app.py
@@ -72,5 +72,4 @@ def predict():
 
 
 if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 8080))
-    app.run(host='0.0.0.0',port = port, ssl_context = 'adhoc')
+    app.run(host='0.0.0.0')
